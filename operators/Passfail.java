9967db8a42eaf25209fb03cc001d4232887ff478
Dhyanesh krishna k.r <157107354+Dhyaneshkrishna@users.noreply.github.com>
2025-12-14 07:04:35 +0530

    Add files via upload

---
+++ operators/Passfail.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Passfail{
+    public static void main(String[]args){
+        Scanner sc= new Scanner(System.in);
+        int a = sc.nextInt();
+        if(a>=50)
+        System.out.println("pass");
+        else 
+        System.out.println("fail");
+    }
+}
---
+++ operators/Twonumequal.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Twonumequal{
+    public static void main(String[]args){
+        Scanner sc= new Scanner(System.in);
+        int n = sc.nextInt();
+        int b = sc.nextInt();
+        if(n==b)
+        System.out.println("true");
+        else
+        System.out.println("False");
+    }
+}
